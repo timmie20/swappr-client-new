def11293be10f46d8dea1680402f7f5db4111d40
timmie20 <olutimilehinolayinka1234@gmail.com>
2025-12-31 12:40:12 +0100

    (update): added loading state for oauth trigger

--- src/components/auth/google-oauth.tsx
+++ src/components/auth/google-oauth.tsx
@@ -1,27 +1,40 @@
 "use client";
+import { useState } from "react";
 import { useSignUp } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import { OAuthStrategy } from "@clerk/types";
 
 export function GoogleSignUpButton() {
-  const { signUp } = useSignUp();
+  const { signUp, isLoaded } = useSignUp();
+  const [isLoading, setIsLoading] = useState(false);
 
   if (!signUp) return null;
 
   const signUpWith = async (strategy: OAuthStrategy) => {
-    await signUp.authenticateWithRedirect({
-      strategy,
-      redirectUrl: "/auth/sign-up/sso-callback", // ✅ Correct
-      redirectUrlComplete: "/auth/sign-up/callback", // ✅ Change to your callback page
-    });
+    if (!isLoaded || isLoading) return;
+
+    try {
+      setIsLoading(true);
+      await signUp.authenticateWithRedirect({
+        strategy,
+        redirectUrl: "/auth/sign-up/sso-callback", // ✅ Correct
+        redirectUrlComplete: "/auth/sign-up/callback", // ✅ Change to your callback page
+      });
+    } catch (error) {
+      console.error("OAuth error:", error);
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <Button
       onClick={() => signUpWith("oauth_google")}
-      className="w-full"
+      className="w-full disabled:cursor-not-allowed"
       variant="outline"
       size="lg"
+      disabled={!isLoaded || isLoading}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -31,7 +44,8 @@ export function GoogleSignUpButton() {
       >
         <path d="M564 325.8C564 467.3 467.1 568 324 568C186.8 568 76 457.2 76 320C76 182.8 186.8 72 324 72C390.8 72 447 96.5 490.3 136.9L422.8 201.8C334.5 116.6 170.3 180.6 170.3 320C170.3 406.5 239.4 476.6 324 476.6C422.2 476.6 459 406.2 464.8 369.7L324 369.7L324 284.4L560.1 284.4C562.4 297.1 564 309.3 564 325.8z" />
       </svg>
-      Continue with Google
+
+      {isLoading ? "Redirecting to Google..." : "Continue with Google"}
     </Button>
   );
 }
