8740a1a58022de5c87a7ee3d293628ed536189ee
timmie20 <olutimilehinolayinka1234@gmail.com>
2026-01-01 19:42:39 +0100

    (fix): update zod import syntax for v4 compatibility with @hookform/resolvers

--- src/components/auth/sign-up-form.tsx
+++ src/components/auth/sign-up-form.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
+import { z } from "zod";
 import { FormInput } from "@/components/forms/form-input";
 import { Button } from "@/components/ui/button";
 import { FieldGroup, Field } from "@/components/ui/field";
