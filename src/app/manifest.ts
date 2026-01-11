dec622a48c864f75f8ee714cd4578483f2266ff9
timmie20 <olutimilehinolayinka1234@gmail.com>
2026-01-11 17:45:05 +0100

    fix: manifest theme color

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#3B82FD",
+    theme_color: "#ffffff",
     icons: [
       {
         src: "/assets/favicon/android-chrome-192x192.png",
