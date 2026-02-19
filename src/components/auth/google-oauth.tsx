6fecec5e9041744c26f1cf7133d761da059dd3a3
timmie20 <olutimilehinolayinka1234@gmail.com>
2026-02-19 11:49:32 +0100

    ui: add spinner to google-oauth button

--- src/components/auth/google-oauth.tsx
+++ src/components/auth/google-oauth.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Button } from "../ui/button";
+import { Spinner } from "../ui/spinner";
 
 export function GoogleSignUpButton() {
   const [initiate, setInitiate] = useState(false);
@@ -35,7 +36,8 @@ export function GoogleSignUpButton() {
         <path d="M564 325.8C564 467.3 467.1 568 324 568C186.8 568 76 457.2 76 320C76 182.8 186.8 72 324 72C390.8 72 447 96.5 490.3 136.9L422.8 201.8C334.5 116.6 170.3 180.6 170.3 320C170.3 406.5 239.4 476.6 324 476.6C422.2 476.6 459 406.2 464.8 369.7L324 369.7L324 284.4L560.1 284.4C562.4 297.1 564 309.3 564 325.8z" />
       </svg>
 
-      {initiate ? "Redirecting to Google..." : "Continue with Google"}
+      {initiate ? (<><Spinner /> <span className="ml-2">Please wait...</span></>) : "Continue with Google"}
+  
     </Button>
   );
-}
+} 
